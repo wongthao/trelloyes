b41dcb9bbe9847cd88a611b79e5b7b50d98d8928
Wong Thao <wongthao12@gmail.com>
2020-06-08 19:00:52 -0500

    first commit

---
+++ src/List.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Card from './Card';
+import './List.css';
+
+function List(props) {
+    return(
+    
+    <section class="List">
+          <header class="List-header">
+            <h2>{props.header}</h2>
+          </header>
+          <div class="List-cards">
+          {props.cards.map((card) =>
+            <Card
+              key={card.id}
+              title={card.title}
+              content={card.content}
+          />
+        )}
+        <button
+          type='button'
+          className='List-add-button'
+        >
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  )
+}
+
+
+
+export default List;
